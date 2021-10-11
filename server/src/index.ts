83acd4b915842327e0523b20793197c8d9d1dd6e
David Ramirez Monroy <ramirezmonroyd@gmail.com>
2021-10-11 14:44:48 -0500

    Deleted some dependencies
    - Body parser was deleted.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,17 @@
 import express, { Application } from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 const app: Application = express();
 require("./config/database");
 
-app.set("port", process.env.port || "4000");
-
 app.use(express.json());
+app.use(cors());
 app.use(morgan("tiny"));
+
 app.use("/", require("./routes/products.routes"));
 
+app.set("port", process.env.port || "4000");
 app.listen(app.get("port"), () => {
   console.log("💻 Server running on port:", app.get("port"));
 });
