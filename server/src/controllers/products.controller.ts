88468d05ec610eb41937b90552cac06e30b02cbf
David Ramirez Monroy <ramirezmonroyd@gmail.com>
2022-03-19 17:03:19 -0500

    Updated some product backend features

--- server/src/controllers/products.controller.ts
+++ server/src/controllers/products.controller.ts
@@ -1,23 +1,22 @@
 import { Request, Response } from "express";
 import Product from "../models/products.model";
 
+export async function createProduct(req: Request, res: Response) {
+  const product = new Product(req.body);
+  await product.save();
+  res.json("👍 Saved");
+}
 export async function readProducts(req: Request, res: Response) {
   const products = await Product.find();
   res.json(products);
 }
 
-export async function saveProduct(req: Request, res: Response) {
-  const product = new Product(req.body);
-  await product.save();
-  res.json("👍 Saved");
+export async function updateProduct(req: Request, res: Response) {
+  await Product.findByIdAndUpdate(req.params.id, req.body);
+  res.json("👍 Updated");
 }
 
 export async function deleteProduct(req: Request, res: Response) {
   await Product.findByIdAndDelete(req.params.id);
   res.json("👍 Deleted");
-}
-
-export async function updateProduct(req: Request, res: Response) {
-  await Product.findByIdAndUpdate(req.params.id, req.body);
-  res.json("👍 Updated");
 }
